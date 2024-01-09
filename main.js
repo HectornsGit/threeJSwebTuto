6ce2b6686fd2c34f711caf9151cf67a4203e9e4f
HectornsGit <hector.novoa.segade@gmail.com>
2024-01-09 17:58:07 +0100

    feat main.js adds a torus mesh and an animation to the canvas

--- main.js
+++ main.js
@@ -13,9 +13,31 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.setZ(30);
 
+// Geometry
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6347,
+  wireframe: true,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
 // Renderer
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(sizes.width, sizes.height);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.y += 0.01;
+  torus.rotation.x += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
